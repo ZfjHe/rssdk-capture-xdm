a0471df178402204ef794ed20cfdc5e7e259fe4e
Tom Propst <tompropst@users.noreply.github.com>
2015-12-15 17:20:15 -0700

    Corrected copyright notice

--- RSDataCollector.cpp
+++ RSDataCollector.cpp
@@ -1,22 +1,17 @@
 /******************************************************************************
 Copyright 2015, Intel Corporation All Rights Reserved.
 
-The source code, information and material("Material") contained herein is owned
-by Intel Corporation or its suppliers or licensors, and title to such Material
-remains with Intel Corporation or its suppliers or licensors. The Material
-contains proprietary information of Intel or its suppliers and licensors. The
-Material is protected by worldwide copyright laws and treaty provisions. No
-part of the Material may be used, copied, reproduced, modified, published,
-uploaded, posted, transmitted, distributed or disclosed in any way without
-Intel's prior express written permission. No license under any patent,
-copyright or other intellectual property rights in the Material is granted to
-or conferred upon you, either expressly, by implication, inducement, estoppel
-or otherwise. Any license under such intellectual property rights must be
-express and approved by Intel in writing.
-
-Unless otherwise agreed by Intel in writing, you may not remove or alter this
-notice or any other notice embedded in Materials by Intel or Intel's suppliers
-or licensors in any way.
+Permission is granted to use, copy, distribute and prepare derivative works of
+this software for any purpose and without fee, provided, that the above 
+copyright notice and this statement appear in all copies. Intel makes no 
+representations about the suitability of this software for any purpose. 
+THIS SOFTWARE IS PROVIDED "AS IS." INTEL SPECIFICALLY DISCLAIMS ALL WARRANTIES, 
+EXPRESS OR IMPLIED, AND ALL LIABILITY, INCLUDING CONSEQUENTIAL AND OTHER 
+INDIRECT DAMAGES, FOR THE USE OF THIS SOFTWARE, INCLUDING LIABILITY FOR 
+INFRINGEMENT OF ANY PROPRIETARY RIGHTS, AND INCLUDING THE WARRANTIES OF 
+MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. Intel does not assume any 
+responsibility for any errors which may appear in this software nor any 
+responsibility to update it.
 ******************************************************************************/
 
 /*******************************************************************************
